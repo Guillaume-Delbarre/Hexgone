069a55554cf9be57c47f3a233a503400086b1935
Guillaume Delbarre <guillaume.voolkan@gmail.com>
2023-10-28 13:26:00 +0200

    init main

--- game/main.py
+++ game/main.py
@@ -1,12 +1,18 @@
 import pygame
 pygame.init()
 
-# Generer la fenetre du jey
+# Generer la fenetre du jeu
 pygame.display.set_caption("Hexgone")
-pygame.display.set_mode((1080, 720))
+screen = pygame.display.set_mode((1080, 720))
 
 running=True
 while running :
+
+    # Coloration du fond en bleu
+    screen.fill("Light Blue")
+
+    # Mise à jour de l'écran
+    pygame.display.flip()
 
     # Boucle de test de la fermeture de la fenetre
     for event in pygame.event.get() :
